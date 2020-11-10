fb731be3d3a28ad1e06ac6b794e4af52fdb17867
paras1223 arora <parasarora1224@gmail.com>
2020-11-10 11:55:50 +0530

    enums good practise

---
+++ enums/enums.dart
@@ -0,0 +1,18 @@
+enum Animal { Lion, wolf, snake }
+void main() {
+  Animal val = Animal.wolf;
+  switch (val) {
+    case Animal.snake:
+      print("Snake");
+      break;
+    case Animal.Lion:
+      {
+        print("lion");
+      }
+      break;
+    default:
+      {
+        print("by default");
+      }
+  }
+}
