edb5debd51edc0560acc24a9a6b16d46063c7757
paras arora <parasarora1224@gmail.com>
2021-03-18 23:36:38 +0530

    indentical function check reference only

--- inentical/identical.dart
+++ inentical/identical.dart
@@ -1,12 +1,17 @@
-class Paras {
+class Player {
   int a;
-  Paras(this.a);
+  Player(this.a);
 }
 
 void main() {
-  var obj = Paras(2);
-  var obj2 = Paras(2);
+  var obj = Player(2);
+  var obj2 = Player(2);
   var obj3 = obj;
-  print("check : ${identical(obj, obj3)}");
-  print("check : ${obj == obj2}");
+
+  print("check : ${identical(obj, obj2)}"); // false
+
+  print("check : ${identical(obj, obj3)}"); // true
+  int b = 4;
+  int c = 4;
+  print("check 2 ${identical(b, c)}");
 }
