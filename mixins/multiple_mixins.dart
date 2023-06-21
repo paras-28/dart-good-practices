f84cff310df9986a41dc86904b9ea624509e18fd
Paras Arora <parasarora1224@gmail.com>
2023-06-21 20:29:42 +0530

    mixin update
    mixin update

--- mixins/multiple_mixins.dart
+++ mixins/multiple_mixins.dart
@@ -2,10 +2,12 @@ mixin Swimming {
   void swim() => print("swimming");
 }
 
-class Walking {
+mixin class Walking {
   void walk() => print("walking");
 }
 
+// in dart 2.0 we can add an class with  "with" keyword
+// but after dart 3.0 we need to add mixin keyword in order to use it
 class Human with Walking, Swimming {
   void jump() => print("jumping");
 }
